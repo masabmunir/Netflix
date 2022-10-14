963f81169dbe8a5a4e0dd7e4790c461a1ef26ae7
masabmunir <masabk214@gmail.com>
2022-10-14 20:49:28 +0500

    search bar and icons added on header component

--- Backend/index.js
+++ Backend/index.js
@@ -6,13 +6,16 @@ const mongoose = require('./db/conn');
  // User Path
 const userRoutes = require('./routes/routes');
 // Images Path
-
 const imageModal = require('./dataModals/images.modal');
 const imageRoutes = require('./routes/images.routes');
-
 // Movies Path
 const movieModal = require('./dataModals/movies.modal');
 const movieRoutes = require('./routes/movies.routes');
+// For uploading large images/videos
+
+app.use(express.json({limit: "50mb", extended: true}))
+app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit: 500000}))
+
 
 const port = process.env.port || 8000
 
